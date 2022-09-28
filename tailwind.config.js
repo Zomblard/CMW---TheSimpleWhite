4f3e5a5a85d5cc2f2754c9aa72b5066da56ec9e5
Léo <69589034+Zomblard@users.noreply.github.com>
2022-09-28 19:30:35 +0200

    [ADD] flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,12 @@
 module.exports = {
   content: [
     './public/*.{html,js}',
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
